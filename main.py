86932af9e5007fc215d5b65977fbcecbcb584861
Thaddeus Kalinowski <thad2050306@gmail.com>
2025-05-02 15:35:33 -0400

    make node class and do some stuff with it

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+# From https://www.geeksforgeeks.org/avl-tree-in-python/
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 def main():
-    print("Test")
+    myNode = Node("a")
+    myNode.left = Node("c")
+    myNode.right = Node("z")
+
+    print(f" {myNode.value}")
+    print("/ \\")
+    print(f"{myNode.left.value}  {myNode.right.value}")
 
 
 
